876fe0a54eb0553318caf243fe27a0817d39d75d
Nicholas Miller <nicholasmill157@gmail.com>
2023-01-01 21:56:11 -0500

    fixed and reworked majority of code, cleaning and finishing

---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+//Setting up usable questions
+var questions = [{
+
+    question: 'What can be used to store data locally',
+    option1: 'Local Storage',
+    option2: 'A Function',
+    option3: 'A Variable',
+    option4: 'A String',
+    answer: "1"
+    },
+    {
+    question: 'What language creates structure?',
+    option1: 'JavaScript',
+    option2: 'CSS',
+    option3: 'HTML',
+    option4: 'None',
+    answer: "3" 
+    },
+    {
+    question: 'Which language creates style?',
+    option1: 'Markdown',
+    option2: 'JavaScript',
+    option3: 'HTML',
+    option4: 'CSS',
+    answer: "4" 
+    },
+    {
+    question: 'Which languages can you connect to work together',
+    option1: 'All of the choices',
+    option2: 'JavaScript',
+    option3: 'HTML',
+    option4: 'CSS',
+    answer: "1" 
+    },
+    {
+    question: 'What does CSS stand for?',
+    option1: 'Compact Style Script',
+    option2: 'Cascading Style Sheet',
+    option3: 'Computer Screen Sheet',
+    option4: 'Complex Screen Style',
+    answer: "2" 
+    },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,117 @@
-var startScreen = document.getElementById('start-scrn');
-var startButton = document.getElementById('start-btn');
-var scoreButton = document.getElementById('score-btn');
-var answerBox = document.getElementById('answer-box');
-var questionScreen = document.getElementById('question-box');
-var questionEl = document.getElementById('question');
-var answer1El = document.getElementById('answer1');
-var answer2El = document.getElementById('answer2');
-var answer3El = document.getElementById('answer3');
-var answer4El = document.getElementById('answer4');
-var timeEl = document.querySelector('.time');
-var scoreEl = document.querySelector('.score');
+// Start Screen variables
+var startScreen = document.querySelector(".start-scrn");
+var startBtn = document.querySelector("#start-btn");
 
-var secondsLeft = 60;
+// Question Screen Variables
+var questionScreen = document.querySelector(".question-scrn");
+var optionBtns = document.querySelectorAll(".option");
+var questionDisplay = document.querySelector(".questions")
+
+//Score and Timer
+var scoreDisplay = document.querySelector(".score");
+var timerDisplay = document.querySelector("#timer");
+
+//High Score form Screen
+var hsFormScreen = document.querySelector(".form");
+var input = document.querySelector(".user-input");
+var submitBtn = document.querySelector(".submit");
+var highscoreSection = document.querySelector(".high-scores");
+
+
+//Logical variables
+var currentIndex = 0;
 var score = 0;
-var question1 = 'What can be used to store data locally';
-var question2 = 'What language creates structure?';
-var question3 = 'Which language creates style?';
-var question4 = 'Which languages can you connect to work together';
+var timer = 60;
+var maxHighScores = 10;
+var highScores = 0;
 
-function setTime() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = "Timer: " + secondsLeft;
-      }, 1000);
-}
+//Setting up usable questions
+var questions = [{
 
-startButton.addEventListener('click', startGame);
+    question: 'What can be used to store data locally',
+    options: ['Local Storage', 'A Function', 'A Variable', 'A String'],
+    answer: "Local Storage"
+    },
+    {
+    question: 'What language creates structure?',
+    options: ['JavaScript', 'CSS', 'HTML', 'None'],
+    answer: "HTML"
+    },
+    {
+    question: 'Which language creates style?',
+    options: ['JavaScript', 'CSS', 'HTML', 'Markdown'],
+    answer: "CSS"
+    },
+    {
+    question: 'Which languages can you connect to work together',
+    options: ['JavaScript', 'CSS', 'HTML', 'All of the choices'],
+    answer: 'All of the choices'
+    },
+    {
+    question: 'What does CSS stand for?',
+    options: ['Compact Style Script', 'Cascading Style Sheet', 'Computer Screen Sheet', 'Complex Screen Style'],
+    answer: "Cascading Style Sheet" 
+    },
+];
 
-function startGame(event) {
-    setTime()
+//Start Button
+startBtn.addEventListener('click', function() {
+    //Switches Active Screen
+    startScreen.classList.remove("active");
+    questionScreen.classList.add("active");
 
-    console.log('started');
-    startScreen.classList.add('hidden')
-    questionScreen.classList.remove('hidden');
-    setQuestion1();
+    //Sets the score to 0
+    scoreDisplay.textContent = score;
 
-    answerBox.addEventListener('click', function({
-        if(true) {
-            setQuestion2();
-        }
-    }));
-}
+    //Starts the Questions
+    startTimer();
+    showQuestions();
+});
 
+function showQuestions() {
+    //Displays the current question
+    questionDisplay.textContent = questions[currentIndex].question;
 
+    //Options ready for user input
+    optionBtns.forEach((btn, index) => {
+        btn.textContent = questions[currentIndex].options[index];
 
-function setQuestion1() {
-    questionEl.textContent = question1;
-    answer1El.textContent = 'Local Storage';
-    answer2El.textContent =  'A Function';
-    answer3El.textContent = 'A Variable';
-    answer4El.textContent = 'A String';
-    answer1El = true;
-    answer2El = false;
-    answer3El = false;
-    answer4El = false;
+        btn.addEventListener('click', function() {
+            if (btn.textContent === questions[currentIndex].answer) {
+                score++;
+                scoreDisplay.textContent = score;
+                nextQuestion();
+            } else {
+                timer -= 15;
+            };
+        });
+    });
+};
 
-    if (answer1El.addEventListener('click') === true) {
-        score++;
+function nextQuestion() {
+    if (currentIndex === (questions.length -1)) {
+        initialForm();
     } else {
-        secondsLeft = secondsLeft - 15;
-    }
-}
+        currentIndex++;
 
-function setQuestion2() {
-    questionEl.textContent = question2;
-    answer1El.textContent = 'JavaScript';
-    answer2El.textContent = 'CSS';
-    answer3El.textContent = 'HTML';
-    answer4El.textContent = 'None';
-    answer1El = false;
-    answer2El = false;
-    answer3El = true;
-    answer4El = false;
-}
+        questionDisplay.textContent = questions[currentIndex].question;
 
-function setQuestion3() {
-    questionEl.textContent = question3;
-    answer1El.textContent = 'Markdown';
-    answer2El.textContent =  'HTML';
-    answer3El.textContent = 'JavaScript';
-    answer4El.textContent = 'CSS';
-    answer1El = false;
-    answer2El = false;
-    answer3El = true;
-    answer4El = false;
-}
+        optionBtns.forEach((btn, index) => {
+            btn.textContent = questions[currentIndex].options[index];
+        });
+    };
+};
 
-function setQuestion4() {
-    questionEl.textContent = question4;
-    answer1El.textContent = 'JavaScript';
-    answer2El.textContent =  'HTML';
-    answer3El.textContent = 'CSS';
-    answer4El.textContent = 'All of the Above';
-    answer1El = false;
-    answer2El = false;
-    answer3El = true;
-    answer4El = false;
-}
+function startTimer() {
+    timerDisplay.textContent = timer;
 
+    var timerCount = setInterval(function() {
+        timer--;
+        timerDisplay.textContent = timer;
 
-
+        if (timer <= 0) {
+            clearInterval(timerCount);
+            initialForm();
+        };
+    },1000);
+};
